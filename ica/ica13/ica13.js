d03ec4918f3cf1f46ef5faa2777cea856fa29ab7
Cassidy Bliss <135576061+CassBliss@users.noreply.github.com>
2023-11-29 23:01:53 -0700

    file renames

--- ica/ica13/ica13.js
+++ ica/ica13/ica13.js
@@ -1,102 +1,114 @@
-function randomRGB(){
-    //return randomRGB(${random(0,255)},${random(0,255),${random(0,255)})
+// setup canvas
+
+//grab the canvas element
+const canvas = document.querySelector('canvas');
+
+//define the canvas rendering context
+const ctx = canvas.getContext('2d');
+
+//define the canvas to be the same size as the screen
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
 
-class ball{
-    constructor(x,y,velx,vely,color,size){
-        this.x=x;
-        this.y=y;
-        this.velx=velx;
-        this.vely=vely;
-        this.color=color;
-        this.size=size;
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    //define the parameters to the instance of the ball upon construction
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
+    //states that we want to draw a shape on the canvas
+    ctx.beginPath();
+    //defines what color the shape will be: in this case the ball's color property
+    ctx.fillStyle = this.color;
+    //traces an arc shape on the canvas given the parameters of position, radius, number of degrees
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    //fill in the area the shape takes up with color
+    ctx.fill();
+  }
+
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
     }
-
-    draw(){
-        ctx.beginPath();
-        ctx.fillStyle=this.color;
-        ctx.arc(this.x,this.y,0,2*Math.PI);
-        ctx.fill();
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
     }
-
-    update() {
-        //if the ball is located past the screen 
-        if((this.x +this.size) >= width){
-            //we can change the velocity to move the ball in the negative direction
-            this.velx = -(this.velx);
-        }
-
-        //this handles the left edge
-        if((this.x + this.size <= 0)){
-            this.velx = -(this.velx);
-        }
-
-        //this handles the bottom of the screen
-        if((this.y +this.size) >= height){
-            //we can change the velocity to move the ball in the negative direction
-            this.vely = -(this.vely);
-        }
-
-        //this handles the top of the screen
-        if((this.y + this.size <= 0)){
-            this.vely = -(this.vely);
-        }
-
-        //will still update the position
-        this.x += this.velx;
-        this.y += this.vely;
-        this.color = randomRGB();
-
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
     }
-
-    collisionDetect(){
-        for(const ball of balls){
-            if (this !==ball) {
-                const dx = this.x -ball.y;
-                const dy = this.y - ball.y;
-                const distance = Math.sqrt(dx*dx + dy*dy);
-
-                if(distance < this.size + ball.size){
-                    ball.color = this.color = randomRGB();
-                }
-            }
-        }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
     }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
 
-    
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
-//end of class declaration
 
 const balls = [];
 
-while(balls.length < 25){
-    const size = random(1,50);
-    const ball = new ball(
-        random(0+size, width+size),
-        random(0+size, height+size),
-        random(-7,7),
-        random(-7,7),
-        randomRGB(),
-        size,
-    );
-    balls.push(ball);
+while (balls.length < 50) {
+  const size = random(1, 50);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
 
-    console.log(balls)
-
+  balls.push(ball);
 }
 
-function loop(){
-    ctx.fillStyle = "rgba(0,0,0,0.25)";
-    ctx.fillRect(0,0,width,height);
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
 
-    for(const ball of balls){
-        ball.draw();
-        ball.update;
-    }
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
 
-    requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 loop();
-
